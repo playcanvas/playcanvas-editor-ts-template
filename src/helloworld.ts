85d132621d9ae5ab65ed9860c0fc8d0d705f6576
nikitka <nikita.osyak@gmail.com>
2023-01-09 14:01:01 +0300

    added global.d.ts to allow proper intellisense in editor

--- src/helloworld.ts
+++ src/helloworld.ts
@@ -1,10 +1,13 @@
 class HelloWorld extends pc.ScriptType {
     text: string;
+    entityLink : pc.Entity;
 
     initialize() {
         console.log('Hello ' + this.text);
+        console.log('Hello entity' + this.entityLink) // autocompletes fine
     }
 };
 
 pc.registerScript(HelloWorld, 'helloWorld');
 HelloWorld.attributes.add('text', {type: 'string'});
+HelloWorld.attributes.add('entityLink', {type : 'entity'})
